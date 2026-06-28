f6bd41e42d66cb4e10c6d051b862692752f1dfb9
agatadonofrio <127829929+agatadonofrio@users.noreply.github.com>
2026-06-28 19:19:13 +0200

    Add files via upload

---
+++ R/sci_step2.R
@@ -0,0 +1,103 @@
+# ANSI color codes
+RED    <- '\033[91m'
+WHITE  <- '\033[97m'
+YELLOW <- '\033[93m'
+ORANGE <- '\033[38;5;208m'
+GREEN  <- '\033[92m'
+RESET  <- '\033[0m'
+
+cat_col <- function(..., color = WHITE) {
+  cat(color, ..., RESET, '\n', sep = '')
+}
+
+usage_str <- paste0(paste0("\033[93m<workdir>", RESET), ' ', paste0("\033[93m<scratch>", RESET))
+
+args_raw <- commandArgs(trailingOnly = TRUE)
+
+if (length(args_raw) != 2) {
+  cat(WHITE, 'Usage: Rscript sci_step2.R ', usage_str, RESET, '\n\n', sep = '')
+  cat_col("Used in combination with SCI_1, from fastq files produces gene expression matrix and QC plots and statistics for SCI data", color = YELLOW)
+  cat('\n')
+  cat_col('Arguments:', color = WHITE)
+  cat('\033[93mworkdir         [io]  Path to working directory containing scratch folder', RESET, '\n', sep = '')
+  cat('\033[93mscratch         [io]  path of the directory output of the sci_step1 function', RESET, '\n', sep = '')
+  quit(status = 1)
+}
+
+# Parse positional arguments
+args <- list()
+args$workdir <- args_raw[1]
+args$scratch <- args_raw[2]
+
+# --- Input validation ---
+errors <- character(0)
+
+if (!dir.exists(args$workdir)) {
+  errors <- c(errors, paste0('Directory not found: workdir = ', args$workdir))
+}
+if (!dir.exists(args$scratch)) {
+  errors <- c(errors, paste0('Directory not found: scratch = ', args$scratch))
+}
+
+if (length(errors) > 0) {
+  for (e in errors) cat(RED, 'ERROR: ', RESET, WHITE, e, RESET, '\n', sep = '')
+  quit(status = 1)
+}
+
+# --- Scratch directory setup ---
+n <- 1
+repeat {
+  if (dir.exists(file.path(normalizePath(args$workdir), paste0('scratch', n)))) {
+    n <- n + 1
+  } else {
+    break
+  }
+}
+
+scratch_path <- file.path(normalizePath(args$workdir), paste0('scratch', n))
+dir.create(scratch_path, recursive = TRUE, showWarnings = FALSE)
+
+# --- Build docker volume mounts ---
+mounts      <- character(0)
+docker_vals <- list()
+service_idx <- 1
+
+mounts <- c(mounts, paste0('-v "', scratch_path, ':/workDir"'))
+docker_vals$workdir <- '/workDir'
+
+# scratch: read-write directory [io]
+mounts <- c(mounts, paste0('-v "', normalizePath(args$scratch), ':/data/scratch"'))
+docker_vals$scratch <- '/data/scratch'
+
+# --- Bind files and service volumes ---
+mounted_folders <- list()
+
+
+# --- Assemble docker command ---
+mount_str <- paste(mounts, collapse = ' ')
+cmd <- paste('docker run --rm', mount_str, 'repbioinfo/monocle Rscript /home/endpipeline.R ')
+placeholders <- regmatches(cmd, gregexpr('<[^>]+>', cmd))[[1]]
+for (ph in placeholders) {
+  key <- gsub('<|>', '', ph)
+  val <- docker_vals[[key]]
+  if (!is.null(val)) cmd <- gsub(ph, val, cmd, fixed = TRUE)
+}
+cat('\n', YELLOW, 'Running:\n', RESET, WHITE, cmd, RESET, '\n\n', sep = '')
+log_path <- file.path(scratch_path, 'output_log.txt')
+cat(YELLOW, 'Log: ', RESET, WHITE, log_path, RESET, '\n\n', sep = '')
+
+con <- file(log_path, open = 'w')
+p   <- pipe(paste(cmd, '2>&1'), open = 'r')
+while (length(line <- readLines(p, n = 1, warn = FALSE)) > 0) {
+  cat(line, '\n', sep = '')
+  writeLines(line, con)
+}
+ret <- close(p)
+close(con)
+
+if (ret == 0) {
+  cat('\n', GREEN, 'Done. Log saved to: ', log_path, RESET, '\n', sep = '')
+} else {
+  cat('\n', RED, 'Docker exited with code ', ret, '. See log: ', log_path, RESET, '\n', sep = '')
+}
+quit(status = ret)
